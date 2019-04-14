656a048b2c5116ec6304e067d1fb4b83d3306afd
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-04-13 21:10:59 -0400

    add scrutinybfl

---
+++ Source/EJ_Enviornment/ScrutinyBFL.h
@@ -0,0 +1,17 @@
+// Copyright Ben Hoffman 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ScrutinyBFL.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EJ_ENVIORNMENT_API UScrutinyBFL : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
